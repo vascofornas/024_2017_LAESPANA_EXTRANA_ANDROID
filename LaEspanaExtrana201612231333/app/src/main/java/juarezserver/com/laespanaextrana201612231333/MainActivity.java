2028433defad4b0ad27b2cdd93da83090ba1565e
vascofornas <desarrollo@webjuarez.com>
2016-12-23 14:08:46 -0700

    001. PROYECTO EN BLANCO - 201612231407

---
+++ LaEspanaExtrana201612231333/app/src/main/java/juarezserver/com/laespanaextrana201612231333/MainActivity.java
@@ -0,0 +1,13 @@
+package juarezserver.com.laespanaextrana201612231333;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
