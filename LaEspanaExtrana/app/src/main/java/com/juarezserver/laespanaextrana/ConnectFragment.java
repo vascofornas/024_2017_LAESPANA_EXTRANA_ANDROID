d51e59def847c6ab102e760dc355dbcf0bb82012
vascofornas <desarrollo@webjuarez.com>
2016-12-24 12:14:32 -0700

    201612241213 ACTUALIZADO ANDROID STUDIO Y INTEGRADO FIREBASE

--- LaEspanaExtrana/app/src/main/java/com/juarezserver/laespanaextrana/ConnectFragment.java
+++ LaEspanaExtrana/app/src/main/java/com/juarezserver/laespanaextrana/ConnectFragment.java
@@ -9,16 +9,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
 
 public class ConnectFragment extends Fragment {
 
+    private Button mSendData;
+    private Firebase mRef;
+
     public ConnectFragment() {
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_connect, container, false);
+
+
+        Firebase.setAndroidContext(this.getActivity());
+
+        mRef = new Firebase("https://la-espana-extrana.firebaseio.com/");
+
+        mSendData = (Button) rootView.findViewById(R.id.sendData);
+
+        mSendData.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getActivity(), "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return rootView;
     }
